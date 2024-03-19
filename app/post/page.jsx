b3b439daf914801124631b1e08bb865e6221e040
Hecoal <hectorcortez_2002@hotmail.com>
2024-03-18 21:17:55 -0600

    Add global CSS styles and update dependencies

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -1,17 +1,18 @@
 import PostCard from "../../components/PostCard";
 import React from "react";
+import './post.css'
 async function loadPosts(){
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
 
-    await new Promise((resolve)=>setTimeout(resolve, 2000));
+    //await new Promise((resolve)=>setTimeout(resolve, 2000));
     return data;
 }
 async function PostPages(){
     const posts = await loadPosts();
     console.log(posts); //Post is an array!
     return (
-        <div>
+        <div className="grid">
             {
                 posts.map((post) => (
                     <PostCard post={post} key={post.id}/>
